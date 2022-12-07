3e776e4d4a101961f848389cf0558d7ea8125a31
ccaruso58 <cc58@njit.edu>
2022-12-06 20:37:39 -0500

    Edited recommendation client file

--- recClient.php
+++ recClient.php
@@ -1,5 +1,20 @@
 #!/usr/bin/php
 <?php
+session_start();
+  if(!isset($_SESSION['valid']) OR $_SESSION['valid'] !== true){
+      header("location: login.php");
+      exit;
+  }
+  if(isset($_SESSION['response'])){
+      $rep = $_SESSION['response'];
+
+      $uname = $rep[0];
+      $fname = $rep[1];
+      $lname = $rep[2];
+      $email = $rep[3];
+      include "nav.php";
+  }
+
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
@@ -24,6 +39,7 @@ $response = $client->send_request($request);
 //$response = $client->publish($request);
 
 $_SESSION['recData'] = $response;
+
 	header("location: Frontend/userRec.php");
         exit;
 
