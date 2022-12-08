3e112deced8083058124958fe0dd3fc1fa6f45a4
ali-raza-it <ali.raza.esmailzada@gmail.com>
2022-12-07 23:05:57 -0500

    conn file fixed

--- Frontend/forum/conn.php
+++ Frontend/forum/conn.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$db_username = 'testUser';
+$db_username = 'ali';
 $db_password = '12345';
 $conn = new PDO( 'mysql:host=localhost;dbname=testdb', $db_username, $db_password );
 if(!$conn){
