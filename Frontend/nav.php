f51fbac242f9203c28a73cb6998be2189eba47cd
ali-raza-it <ali.raza.esmailzada@gmail.com>
2022-12-08 21:25:03 -0500

    minor appearance changes

--- Frontend/nav.php
+++ Frontend/nav.php
@@ -19,9 +19,9 @@
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
+  
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -53,12 +53,9 @@
       </div>
     </form>
         <ul class="nav navbar-nav navbar-right">
-        
-		
 		<li><a onclick="myFunction()" class="w3-dropdown-click"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
   <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
 </svg> Notifications <div class="w3-container">
-		  
 		    <div id="Demo" class="w3-dropdown-content w3-bar-block w3-border">
 		      <a href="#" class="w3-bar-item w3-button">Link 1</a>
 		      <a href="#" class="w3-bar-item w3-button">Link 2</a>
